fed83387c6c6b909846728e8c25bdfa5515917cd
Arion John <ajohn@brooklynsteamcenter.org>
2024-09-16 10:48:58 -0400

    Python Lesson #1

---
+++ lesson_00.py
@@ -0,0 +1,30 @@
+print('arion john') 
+#Step 2
+fav_show = "KC Undercover" 
+#Step 3 
+float_number = 10934.304 
+int_number = 12
+#Step 4 
+print(float_number + int_number) 
+#Step 5 Output
+Fav_Foods = "McDonalds", "Honey Bun", "Oreos"
+#Step 6 
+
+age = int(input ("What is your age?"))
+if age >= 16:
+    print("You are old enough to  take your drivers license")
+else:
+    print("You can't take your drivers license yet")
+#Step 7 Drivers License 
+
+name = input("What is your name?")
+#Step 8 
+
+for x in Fav_Foods:
+    print(x)
+#Step 9 For Loop
+
+wontrun_loop = -1
+while wontrun_loop > 0:
+    print("I will never run")
+#Step 10 While Loop will no intentions of running
