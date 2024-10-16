b8a36ae2421b1838e8223d4cce324a5d683eb831
Arion John <ajohn@brooklynsteamcenter.org>
2024-10-16 10:41:57 -0400

    Debugging

--- 09_debugging.py
+++ 09_debugging.py
@@ -16,3 +16,29 @@ for num in range(1, 20):
     else:
         print(num, "is odd.")
 
+num = int(input("Enter an integer: "))
+if num < -1:
+  print("No negative numbers.")
+else:
+  result = 1
+  for i in range(1, num + 1):
+    result *= i   
+
+  print("Factorial of " + str(num) + " is " + str(result))
+
+attempts = 0
+correct_password = "secret"
+
+while True:
+    password = input("Enter your password: ")
+
+    if password == correct_password:
+        print("Correct password!")
+        break
+    else:
+        print("Incorrect password")
+
+    if attempts > 3:
+        print("Too many attempts")
+        break
+    attempts += 1
